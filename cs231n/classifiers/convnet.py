ddf0181dd2a8eb363e9eccef13df6bffd7d03d13
Jody-Lu <b00901192@ntu.edu.tw>
2017-01-16 18:32:08 -0600

    add new networks

---
+++ cs231n/classifiers/convnet.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from cs231n.layers import *
+from cs231n.fast_layers import *
+from cs231n.layer_utils import *
+
+class MultiLayerConvNet(object):
+  """
+  [conv-relu-pool]xN - conv - relu - [affine]xM - [softmax or SVM
+  """
+  pass
+
+class MultiLayerDeepConvNet(object):
+  """
+  [conv-relu-pool]XN - [affine]XM - [softmax or SVM]
+  """
